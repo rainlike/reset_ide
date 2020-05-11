43364ac1d04ac179eb89eaba913cc5fca88293c7
10riy <yura.gagarin9@gmail.com>
2020-05-11 16:01:57 +0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reset ide
+
+## Help
+```
+$ python3 reset_ide_2020.py -h
+```
+
+## Example use
+```
+$ python3 reset_ide_2020.py -u john -n datagrip
+```
---
+++ reset_ide_2020.py
@@ -0,0 +1,97 @@
+import os
+import re
+import argparse
+import subprocess
+import shutil
+
+def get_versions(path, ide_prefix):
+    versions = []
+    for directory in os.listdir(path):
+        if re.match(rf'^{ide_prefix}*', directory):
+            versions.append(directory)
+    return versions
+
+def get_current_version(path, ide_prefix):
+    result = ''
+    versions = get_versions(config_path, ide_prefix)
+    if (len(versions) > 0):
+        versions.sort()
+        result = versions[-1]
+    return result
+
+def remove_files_by_pattern(dir_path, pattern):
+    for f in os.listdir(dir_path):
+        if re.search(pattern, f):
+            os.remove(os.path.join(dir_path, f))
+
+def remove_lines_by_pattern(file_name, pattern):
+    updateLines = []
+    with open(file_name, 'r') as file:
+        lines = open(file_name, 'r').readlines()
+        for line in lines:
+            if re.search(pattern, line):
+                continue
+            updateLines.append(line)
+    
+    if len(updateLines) > 0:
+        with open(file_name, 'w') as file:
+            file.writelines(updateLines)
+
+parser = argparse.ArgumentParser(description='Reset IDE key')
+parser.add_argument(
+    '-u', '--username', metavar='U', type=str, required=True,
+    help='username')
+
+parser.add_argument(
+    '-n', '--name', metavar='N', type=str, required=False, default='phpstorm',
+    help='ide name, example: phpstorm')
+
+args = parser.parse_args()
+username = args.username
+ide_name=args.name
+
+ide_prefix = ''
+if ide_name == 'clion':
+    ide_prefix = 'CLion'
+elif ide_name == 'goland':
+    ide_prefix = 'GoLand'
+elif ide_name == 'phpstorm':
+    ide_prefix = 'PhpStorm'
+elif ide_name == 'datagrip':
+    ide_prefix = 'DataGrip'
+
+if (not ide_prefix):
+    print('Not found support ide')
+    exit()
+
+config_path = f'/home/{username}/.config/JetBrains'
+
+current_version = get_current_version(config_path, ide_prefix)
+if (not current_version):
+    print('Not found current ide version')
+    exit()
+
+config_ide_path = '{}/{}'.format(config_path, current_version)
+
+try:
+    evaluation_folder = '{}/eval/'.format(config_ide_path)
+    remove_files_by_pattern(evaluation_folder, '([a-zA-Z0-9]+)\.evaluation.key')
+except Exception as e:
+    print('Not remove evaluation keys, error: ' + str(e))
+    exit()
+
+try:
+    other_xml = '{}/options/other.xml'.format(config_ide_path)
+    remove_lines_by_pattern(other_xml, 'evlsprt')
+except Exception as e:
+    print('Not remove lines in other.xml, error: ' + str(e))
+    exit()
+
+try:
+    ide_java_conf = f'/home/{username}/.java/.userPrefs/jetbrains/{ide_name}/'
+    shutil.rmtree(ide_java_conf)
+except Exception as e:
+    print('Not remove java settings, error: ' + str(e))
+    exit()
+
+print(f'Done! Reset {ide_name}!')
